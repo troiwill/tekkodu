f371e28549a6adce707baf046ee0f302ce6f6b2a
troiwilliams <twilliams1903@gmail.com>
2013-08-27 01:07:54 -0400

    Added the isSameTypeAs(...) function

--- Primitives/KoduPrimitive.h
+++ Primitives/KoduPrimitive.h
@@ -6,49 +6,52 @@
 #include <string>
 
 namespace Kodu {
-	
-	//! Kodu Primitive
-	class KoduPrimitive {	
-	public:
-		//! Constructor
-		KoduPrimitive(const std::string& kPrimitiveType)
-		  : primitiveType(kPrimitiveType)
-		{ }
-		
-		//! Copy constructor
-		KoduPrimitive(const KoduPrimitive& kBehavior)
-		  : primitiveType(kBehavior.primitiveType)
-		{ }
+    
+    //! Kodu Primitive
+    class KoduPrimitive {   
+    public:
+        //! Constructor
+        KoduPrimitive(const std::string& kPrimitiveType)
+          : primitiveType(kPrimitiveType)
+        { }
+        
+        //! Copy constructor
+        KoduPrimitive(const KoduPrimitive& kBehavior)
+          : primitiveType(kBehavior.primitiveType)
+        { }
 
-		//! Destructor
-		virtual ~KoduPrimitive() {
-			// no explicit implementation
-		}
-		
-		//! Assignment operator
-		KoduPrimitive& operator=(const KoduPrimitive& kBehavior) {
-			if (this != &kBehavior) {
-				primitiveType = kBehavior.primitiveType;
-			}
-			return *this;
-		}
+        //! Destructor
+        virtual ~KoduPrimitive() {
+            // no explicit implementation
+        }
+        
+        //! Assignment operator
+        KoduPrimitive& operator=(const KoduPrimitive& kBehavior) {
+            if (this != &kBehavior) {
+                primitiveType = kBehavior.primitiveType;
+            }
+            return *this;
+        }
 
-		//! Returns the name of the derived primitive
-		const std::string& getPrimitiveType() const {
-			return primitiveType;
-		}
-		
-		//! Used to reinitialize certain variables (e.g. when switching to another page)
-		virtual void reinitialize() = 0;
+        //! Returns the name of the derived primitive
+        const std::string& getPrimitiveType() const {
+            return primitiveType;
+        }
+        
+        //! Tests if the primitive argument is the same as the calling class
+        static bool isSameTypeAs(const KoduPrimitive*);
 
-		//! Prints the attributes for a particular primitive (pure virtual function)
-		virtual void printAttrs() {
-			std::cout << "Primitive Type: " << primitiveType << std::endl;
-		}
+        //! Used to reinitialize certain variables (e.g. when switching to another page)
+        virtual void reinitialize() = 0;
 
-	protected:
-		std::string primitiveType;	//!< The name of the derived primitive
-	};
+        //! Prints the attributes for a particular primitive (pure virtual function)
+        virtual void printAttrs() const {
+            std::cout << "Primitive Type: " << primitiveType << std::endl;
+        }
+
+    protected:
+        std::string primitiveType;  //!< The name of the derived primitive
+    };
 } // end of Kodu namespace
 
 #endif // KODU_PRIMITIVE_H_
