f378dff963540ebd2787afa44c11adf5f7bb73bb
troiwilliams <twilliams1903@gmail.com>
2013-08-28 13:13:16 -0400

    Changed indentation style (tabs -> spaces)

--- General/GeneralMacros.h
+++ General/GeneralMacros.h
@@ -5,7 +5,7 @@
 
 //! A printing shortcut for boolean attributes
 #define PRINT_ATTRS(AttrName, BooleanVal) \
-	std::cout << AttrName << ": " << (BooleanVal ? "true" : "false") << std::endl;
+    std::cout << AttrName << ": " << (BooleanVal ? "true" : "false") << std::endl;
 
 //! Disallows the copy constructor
 #define DISALLOW_COPY(TypeName) TypeName(const TypeName&)
@@ -14,14 +14,14 @@
 #define DISALLOW_ASSIGN(TypeName) TypeName& operator=(const TypeName&)
 
 //! Disallows both the copy constructor and the assignment operator
-#define DISALLOW_COPY_ASSIGN(TypeName)	\
-			DISALLOW_COPY(TypeName);	\
-			DISALLOW_ASSIGN(TypeName)
+#define DISALLOW_COPY_ASSIGN(TypeName)  \
+            DISALLOW_COPY(TypeName);    \
+            DISALLOW_ASSIGN(TypeName)
 
 //! Disallows a class from being instantiated
-#define DISALLOW_INSTANTIATION(TypeName)	\
-			DISALLOW_COPY_ASSIGN(TypeName);	\
-			TypeName();						\
-			~TypeName();
+#define DISALLOW_INSTANTIATION(TypeName)    \
+            DISALLOW_COPY_ASSIGN(TypeName); \
+            TypeName();                     \
+            ~TypeName();
 
 #endif // GENERAL_MACROS_H_
