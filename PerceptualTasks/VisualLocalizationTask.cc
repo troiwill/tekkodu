f4b492a99863d1c3c2365b23303cc25aaec3201a
troiwilliams <twilliams1903@gmail.com>
2013-11-15 20:13:35 -0500

    Removed unnecessary code---the localization task only uses the constellation to localize

--- PerceptualTasks/VisualLocalizationTask.cc
+++ PerceptualTasks/VisualLocalizationTask.cc
@@ -19,43 +19,52 @@ namespace Kodu {
     unsigned int VisualLocalizationTask::idCount = 50000;
 
     bool VisualLocalizationTask::canExecute(const KoduWorld& kWorldState) {
-        return (!kWorldState.thisAgent.isWalking());
+        if (localizationPoints.size() < 2) {
+            std::cout << "!!! ERROR! the localizationPoints map is empty!\n";
+            return false;
+        }
+        return (!kWorldState.thisAgent.isWalking() && !localizationPoints.empty());
     }
     
     const DualCoding::PilotRequest& VisualLocalizationTask::getPilotRequest() {
         std::cout << "[Visual Localization Task]\n";
-        const unsigned int kMinAmountOfShapesToLocalize = 2;
+        // construct the mapbuilder request
         DualCoding::MapBuilderRequest* mreq = NULL;
         mreq = new DualCoding::MapBuilderRequest(DualCoding::MapBuilderRequest::localMap);
-
+        mreq->removePts = false;
+        
         if (!localizationPoints.empty()) {
+            // the vector that will contain the position of the stars the robot should look at
             std::vector<DualCoding::Point> starLocs;
             starLocs.reserve(localizationPoints.size());
             std::cout << "creating the localization point vector for " << localizationPoints.size()
                 << " points.\n";
 
+            // the amount of stars the robot needs to localize (should be the same value as the
+            // minimum shapes the pilot needs in the localShS to localize)
             const int kNumbOfStarsNeeded = 2;
+            // a count of the number of stars added to the vector
             int numbOfStarsInVector = 0;
+            // iterate over all the stars in the map
             for (std::map<unsigned int, DualCoding::Point>::iterator it = localizationPoints.begin();
                 it != localizationPoints.end(); ++it)
             {
-                //******** to fix... make the robot only look a little back when stars are to the side
                 AngSignPi dtheta = bearingFromAgentToPoint(it->second);
                 dtheta += AngSignPi(M_PI / 2.0f);
                 if (dtheta > 0.0f) {
-                    count++;
                     std::cout << "adding tag #" << it->first << " @ " << it->second << " to vector.\n";
                     NEW_SHAPE(star, DualCoding::AprilTagData,
                         new DualCoding::AprilTagData(DualCoding::VRmixin::worldShS,
                             AprilTags::TagDetection(it->first), DualCoding::Point(it->second)));
                     DualCoding::Point starCen(star->getCentroid());
+                    //******** to fix... make the robot only look a little back when stars are to the side
                     starLocs.push_back(starCen);
                     starLocs.push_back(DualCoding::Point(starCen.coordX() - 200.0f, starCen.coordY(),
                         starCen.coordZ(), starCen.getRefFrameType()));
-                    
+                    //**********
+                    // if there is enough stars in the vector, exit
                     if ((++numbOfStarsInVector) == kNumbOfStarsNeeded) break;
                 }
-                //**********
             }
             std::cout << "generating localization polygon\n";
             NEW_SHAPE(localizePolygon, DualCoding::PolygonData,
@@ -66,48 +75,16 @@ namespace Kodu {
             std::cout << "creating mapbuilder request\n";
             mreq->setAprilTagFamily();
             mreq->searchArea = localizePolygon;
-
-        } else if (!globalGazePolygon.isValid()) {
-            std::cout << "getting the usable objects in the world\n";
-            std::vector<DualCoding::ShapeRoot> usableShapes;
-            usableShapes = DualCoding::subset(DualCoding::VRmixin::worldShS,
-                                              IsShapeOfType(cylinderDataType));
-
-            // calculate the shapes' positions relative to the robot
-            std::vector<DualCoding::Point> localizeGazePoints;
-            std::cout << "generating localization points from shapes\n";
-            for (std::size_t i = 0; i < usableShapes.size(); i++) {
-                DualCoding::ShapeRoot& currShape = usableShapes[i];
-                const DualCoding::Point& kCurrShapePt = currShape->getCentroid();
-                // calculate the bearing to the shape
-                //float bearingToCurrShape = (kCurrShapePt - agentPosition).atanYX();
-                //float dtheta = AngSignPi(bearingToCurrShape - agentOrientation);
-                // calculate the distance to the shape's centroid
-                float dx = kCurrShapePt.coordX() - agentPosition.coordX();
-                float dy = kCurrShapePt.coordY() - agentPosition.coordY();
-                //float h = sqrt((dx * dx) + (dy * dy));
-                localizeGazePoints.push_back(DualCoding::Point(dx, dy, 0.0f, DualCoding::egocentric));
-            }
-            
-            // create the polygon shape
-            std::cout << "generating localization polygon\n";
-            NEW_SHAPE(localizePolygon, DualCoding::PolygonData,
-                new DualCoding::PolygonData(DualCoding::VRmixin::localShS, localizeGazePoints, false));
-            localizePolygon->setObstacle(false);
-            localizePolygon->setViewable(true);
-
-            // build the mapbuilder request
-            std::cout << "creating mapbuilder request\n";
-            for (std::size_t i = 0; i < usableShapes.size(); i++) {
-                mreq->addObjectColor(usableShapes[i]->getType(), usableShapes[i]->getColor());
-            }
-            mreq->searchArea = localizePolygon;
-        } else {
-            mreq->searchArea = globalGazePolygon;
         }
-        //mreq->pursueShapes = true;
-        mreq->removePts = false;
-        
+        // this should never happen because if the localization points map has less than 2 stars, then
+        // the request will never execute.
+        else {
+            std::cout << "THIS SHOULD HAVE NEVER EXECUTED!!! WHAT'S HAPPENING!!!\n";
+            taskStatus = TS_FAILURE;
+            mreq = NULL;
+            return pilotreq;
+        }
+
         // create the pilot request
         std::cout << "creating pilot request\n";
         pilotreq = DualCoding::PilotRequest(DualCoding::PilotTypes::localize);
@@ -118,9 +95,5 @@ namespace Kodu {
         std::cout << "done!\n";
         mreq = NULL;
         return pilotreq;
-    }
-
-    bool VisualLocalizationTask::usedGlobalGazePolygon() const {
-        return globalGazePolygon.isValid();
     }
 }
--- PerceptualTasks/VisualLocalizationTask.h
+++ PerceptualTasks/VisualLocalizationTask.h
@@ -19,36 +19,12 @@ namespace Kodu {
         //! Constructor
         VisualLocalizationTask(const std::map<unsigned int, DualCoding::Point>& kStarConstellation)
           : PerceptualTaskBase(PT_VIS_LOCALIZATION, ++idCount),
-            agentPosition(),
-            agentOrientation(),
-            globalGazePolygon(),
             localizationPoints(kStarConstellation)
-        { }
-
-        //! Constructor
-        VisualLocalizationTask(const DualCoding::Point& kAgentPos, float agentOrient)
-          : PerceptualTaskBase(PT_VIS_LOCALIZATION, ++idCount),
-            agentPosition(kAgentPos),
-            agentOrientation(agentOrientation),
-            globalGazePolygon(),
-            localizationPoints()
-        { }
-
-        //! Constructor
-        VisualLocalizationTask(const DualCoding::Shape<DualCoding::PolygonData>& kGazePolygon)
-          : PerceptualTaskBase(PT_VIS_LOCALIZATION, ++idCount),
-            agentPosition(),
-            agentOrientation(),
-            globalGazePolygon(kGazePolygon),
-            localizationPoints()
         { }
 
         //! Copy constructor
         VisualLocalizationTask(const VisualLocalizationTask& kTask)
           : PerceptualTaskBase(kTask),
-            agentPosition(kTask.agentPosition),
-            agentOrientation(kTask.agentOrientation),
-            globalGazePolygon(kTask.globalGazePolygon),
             localizationPoints(kTask.localizationPoints)
         { }
 
@@ -61,9 +37,6 @@ namespace Kodu {
         VisualLocalizationTask& operator=(const VisualLocalizationTask& kTask) {
             if (this != &kTask) {
                 PerceptualTaskBase::operator=(kTask);
-                agentPosition = kTask.agentPosition;
-                agentOrientation = kTask.agentOrientation;
-                globalGazePolygon = kTask.globalGazePolygon;
                 localizationPoints = kTask.localizationPoints;
             }
             return *this;
@@ -75,15 +48,9 @@ namespace Kodu {
         //! Generates the Pilot request the agent needs to localize
         virtual const DualCoding::PilotRequest& getPilotRequest();
 
-        //! States whether the robot used a gaze polygon passed through the constructor, or a custom one
-        bool usedGlobalGazePolygon() const;
-
     private:
-        static unsigned int idCount;
-        DualCoding::Point agentPosition;
-        float agentOrientation;
-        DualCoding::Shape<DualCoding::PolygonData> globalGazePolygon;
-        std::map<unsigned int, DualCoding::Point> localizationPoints;
+        static unsigned int idCount;    //!< used to create an id for each task
+        std::map<unsigned int, DualCoding::Point> localizationPoints;   //!< a copy of the constellation
     };
 }
 
