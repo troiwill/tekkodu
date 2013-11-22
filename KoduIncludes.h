3576e21776deca414699710e45795924eb5d101e
troiwilliams <twilliams1903@gmail.com>
2013-11-21 19:41:10 -0500

    Includes most of the header files in Kodu library

---
+++ KoduIncludes.h
@@ -0,0 +1,43 @@
+
+/// ===================== KODU PRIMITIVES ===================== ///
+#ifndef KODU_PRIMITIVE_INCLUDES_H_
+#define KODU_PRIMITIVE_INCLUDES_H_
+
+// actions
+#include "Kodu/Primitives/KoduActionGrab.h"
+#include "Kodu/Primitives/KoduActionMotion.h"
+#include "Kodu/Primitives/KoduActionPageSwitch.h"
+#include "Kodu/Primitives/KoduActionSay.h"
+
+// conditions
+#include "Kodu/Primitives/KoduConditionAlways.h"
+#include "Kodu/Primitives/KoduConditionBump.h"
+#include "Kodu/Primitives/KoduConditionSee.h"
+#include "Kodu/Primitives/KoduConditionGot.h"
+
+
+#endif // KODU_PRIMITIVE_INCLUDES_H_
+
+
+/// ===================== PERCEPTUAL TASKS ===================== ///
+#ifndef KODU_PERCEPTUAL_TASKS_INCLUDES_H_
+#define KODU_PERCEPTUAL_TASKS_INCLUDES_H_
+
+#include "Kodu/PerceptualTasks/PerceptualTaskBase.h"
+#include "Kodu/PerceptualTasks/VisualBumpDetectionTask.h"
+#include "Kodu/PerceptualTasks/VisualGripperMonitorTask.h"
+#include "Kodu/PerceptualTasks/VisualLocalizationTask.h"
+#include "Kodu/PerceptualTasks/VisualWalkProgressTask.h"
+
+#endif // KODU_PERCEPTUAL_TASKS_INCLUDES_H_
+
+
+/// ===================== KODU STATES ===================== ///
+#ifndef KODU_STATE_INCLUDES_H_
+#define KODU_STATE_INCLUDES_H_
+
+#include "Kodu/KoduAgent.h"
+#include "Kodu/KoduWorld.h"
+#include "Kodu/PosOrientState.h"
+
+#endif // KODU_STATE_INCLUDES_H_
